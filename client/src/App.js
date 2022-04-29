c65ca30b924b8b35db87933b6e6bc20dcb8241df
Jayson Infante <jayson.infante708@gmail.com>
2022-04-29 00:18:50 -0400

    Adding initial routing for user related things

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Account from "./components/account/Account";
+import Home from "./components/account/Home";
+import Landing from "./components/account/Landing";
+import Navigation from "./components/account/Navigation";
+import SignIn from "./components/account/SignIn";
+import SignUp from "./components/account/SignUp";
+import { AuthProvider } from "./firebase/Auth";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <AuthProvider>
+            <Router>
+                <div className="App">
+                    <div className="App">
+                        <header className="App-header">
+                            <Navigation />
+                        </header>
+                    </div>
+                </div>
+                <Routes>
+                    <Route index element={<Landing />} />
+                    <Route path="landing" element={<Landing />} />
+                    {/* Private route */}
+                    <Route path="/home" element={<Home />} />
+                    {/* Private route */}
+                    <Route path="/account" element={<Account />} />
+                    <Route path="/signin" element={<SignIn />} />
+                    <Route path="/signup" element={<SignUp />} />
+                    <Route
+                        path="*"
+                        element={<p>There's nothing here: 404!</p>}
+                    />
+                </Routes>
+            </Router>
+        </AuthProvider>
+    );
 }
 
 export default App;
