9d58138ff51e0a249e697c5b0d5400feaa03f5ae
bhavya <bshah24@stevens.edu>
2022-05-13 12:13:24 -0400

    restaurant page

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,42 @@
-import React, { useContext } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Account from "./components/account/Account";
-import Home from "./components/account/Home";
-import Landing from "./components/account/Landing";
-import Navigation from "./components/account/Navigation";
-import SignIn from "./components/account/SignIn";
-import SignUp from "./components/account/SignUp";
-import { AuthProvider } from "./firebase/Auth";
+import React, { useContext } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Account from './components/account/Account'
+import Home from './components/account/Home'
+import Landing from './components/account/Landing'
+import Navigation from './components/account/Navigation'
+import SignIn from './components/account/SignIn'
+import SignUp from './components/account/SignUp'
+import Restaurant from './components/restaurant/Restaurant'
+import { AuthProvider } from './firebase/Auth'
 
-import "./App.css";
+import './App.css'
 
 function App() {
-    return (
-        <AuthProvider>
-            <Router>
-                <div className="App">
-                    <div className="App">
-                        <header className="App-header">
-                            <Navigation />
-                        </header>
-                    </div>
-                </div>
-                <Routes>
-                    <Route index element={<Landing />} />
-                    <Route path="landing" element={<Landing />} />
-                    {/* Private route */}
-                    <Route path="/home" element={<Home />} />
-                    {/* Private route */}
-                    <Route path="/account" element={<Account />} />
-                    <Route path="/signin" element={<SignIn />} />
-                    <Route path="/signup" element={<SignUp />} />
-                    <Route
-                        path="*"
-                        element={<p>There's nothing here: 404!</p>}
-                    />
-                </Routes>
-            </Router>
-        </AuthProvider>
-    );
+  return (
+    <AuthProvider>
+      <Router>
+        <div className="App">
+          <div className="App">
+            <header className="App-header">
+              <Navigation />
+            </header>
+          </div>
+        </div>
+        <Routes>
+          <Route index element={<Landing />} />
+          <Route path="landing" element={<Landing />} />
+          {/* Private route */}
+          <Route path="/home" element={<Home />} />
+          {/* Private route */}
+          <Route path="/account" element={<Account />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/restaurant/:restaurantId" element={<Restaurant />} />
+          <Route path="*" element={<p>There's nothing here: 404!</p>} />
+        </Routes>
+      </Router>
+    </AuthProvider>
+  )
 }
 
-export default App;
+export default App
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,13 @@
-const userRoutes = require("./users");
+const userRoutes = require('./users')
 const restaurantRoutes = require('./restaurants')
-const s3Routes = require("./s3");
+const s3Routes = require('./s3')
 
 const constructorMethod = (app) => {
-  app.use("/user", userRoutes);
-  app.use('/restaurants', restaurantRoutes);
-  app.use("/s3", s3Routes);
-  app.use("*", (req, res) => {
+  app.use('/user', userRoutes)
+  app.use('/restaurants', restaurantRoutes)
+  app.use('/s3', s3Routes)
+  app.use('*', (req, res) => {
     res.status(404).json({ error: '404 page Not found' })
-  });
-
-
-module.exports = constructorMethod;
+  })
+}
+module.exports = constructorMethod
