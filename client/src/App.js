1df7830a65034902c328d8ed48430bf2c94fd709
Jayson Infante <jayson.infante708@gmail.com>
2022-05-13 18:54:00 -0400

    Added route for change password

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Account from "./components/account/Account";
+import ChangePassword from "./components/account/ChangePassword";
 import Home1 from "./components/account/Home";
 import Landing from "./components/account/Landing";
 import Navigation from "./components/account/Navigation";
@@ -13,33 +14,47 @@ import Home from "./components/Home";
 import "./App.css";
 
 function App() {
-  return (
-    <AuthProvider>
-      <Router>
-        <div className="App">
-          <div className="App">
-            <header className="App-header">
-              <Navigation />
-            </header>
-          </div>
-        </div>
-        <Routes>
-          <Route index element={<Landing />} />
-          {/*<Route path="landing" element={<Landing />} />*/}
-          {/* Private route */}
-          {/*<Route path="/home" element={<Home />} />*/}
-          {/* Private route */}
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/restaurants" element={<Restaurants />} />
-          <Route path="/account" element={<Account />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/restaurants/:restaurantId" element={<Restaurant />} />
-          <Route path="*" element={<p>There's nothing here: 404!</p>} />
-        </Routes>
-      </Router>
-    </AuthProvider>
-  );
+    return (
+        <AuthProvider>
+            <Router>
+                <div className="App">
+                    <div className="App">
+                        <header className="App-header">
+                            <Navigation />
+                        </header>
+                    </div>
+                </div>
+                <Routes>
+                    <Route index element={<Landing />} />
+                    {/*<Route path="landing" element={<Landing />} />*/}
+                    {/* Private route */}
+                    {/*<Route path="/home" element={<Home />} />*/}
+                    {/* Private route */}
+                    <Route exact path="/" element={<Home />} />
+                    <Route
+                        exact
+                        path="/restaurants"
+                        element={<Restaurants />}
+                    />
+                    <Route path="/account" element={<Account />} />
+                    <Route
+                        path="/changePassword"
+                        element={<ChangePassword />}
+                    />
+                    <Route path="/signin" element={<SignIn />} />
+                    <Route path="/signup" element={<SignUp />} />
+                    <Route
+                        path="/restaurants/:restaurantId"
+                        element={<Restaurant />}
+                    />
+                    <Route
+                        path="*"
+                        element={<p>There's nothing here: 404!</p>}
+                    />
+                </Routes>
+            </Router>
+        </AuthProvider>
+    );
 }
 
 export default App;
