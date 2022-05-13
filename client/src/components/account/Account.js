f4f26c84911d3d4f1b739034b62f855d9858dec9
Jayson Infante <jayson.infante708@gmail.com>
2022-05-13 18:54:16 -0400

    Added user details to profile page

--- client/src/components/account/Account.js
+++ client/src/components/account/Account.js
@@ -1,13 +1,47 @@
-import React from "react";
-import SignOutButton from "./SignOut";
-import "../../App.css";
+import React, { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 import ChangePassword from "./ChangePassword";
+import SignOutButton from "./SignOut";
+import { AuthContext } from "../../firebase/Auth";
+
+import "../../App.css";
 
 const Account = () => {
+    const [user, setUser] = useState(null);
+    const { currentUser } = useContext(AuthContext);
+    console.log(currentUser.uid);
+
+    useEffect(() => {
+        async function getUser() {
+            try {
+                let { data } = await axios.get(
+                    `http://localhost:3001/user/userDetails/${currentUser.uid}`
+                );
+
+                setUser(data);
+            } catch (e) {
+                console.log(e);
+            }
+        }
+        getUser();
+    }, []);
+
+    // let pastOrders = user.pastOrders;
+
+    console.log(user);
     return (
         <div>
             <h2>Account Page</h2>
-            <ChangePassword />
+
+            <h3>{user.name}</h3>
+            <img src={user.image_url} alt="profile picture"></img>
+            <p>Address: {user.address}</p>
+            <p>About me: {user.biography}</p>
+            <Link to="/changePassword" className="link">
+                Change Password
+            </Link>
+            {/* <ChangePassword /> */}
             <SignOutButton />
         </div>
     );
