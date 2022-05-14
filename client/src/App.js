17cde5d793a5650d0d99e3ef6a4ddfb5dc68606f
AnkitKachhadiya <28569675+AnkitKachhadiya@users.noreply.github.com>
2022-05-14 10:46:53 -0400

    Admin routes

--- client/src/App.js
+++ client/src/App.js
@@ -6,13 +6,9 @@ import Landing from "./components/account/Landing";
 import Navigation from "./components/account/Navigation";
 import SignIn from "./components/account/SignIn";
 import SignUp from "./components/account/SignUp";
-import AdminSignin from "./components/admin/AdminSignin";
-import AdminRestaurant from "./components/admin/AdminRestaurant";
 import { AuthProvider } from "./firebase/Auth";
-
 import "./App.css";
-import FoodItemEdit from "./components/admin/FoodItemEdit";
-import FoodItemAdd from "./components/admin/FoodItemAdd";
+import AdminRoutes from "./components/admin/AdminRoutes";
 
 function App() {
     return (
@@ -32,19 +28,7 @@ function App() {
                     <Route path="/account" element={<Account />} />
                     <Route path="/signin" element={<SignIn />} />
                     <Route path="/signup" element={<SignUp />} />
-                    <Route path="/admin/signin" element={<AdminSignin />} />
-                    <Route
-                        path="/admin/restaurant"
-                        element={<AdminRestaurant />}
-                    />
-                    <Route
-                        path="/admin/restaurant/foodItem/Edit/:foodItemId"
-                        element={<FoodItemEdit />}
-                    />
-                    <Route
-                        path="/admin/restaurant/foodItem/Add"
-                        element={<FoodItemAdd />}
-                    />
+                    <Route path="/admin/*" element={<AdminRoutes />} />
                     <Route
                         path="*"
                         element={<p>There's nothing here: 404!</p>}
