a2c1b06a018a5d5da401ceb35c85355753a82e1d
Jayson Infante <jayson.infante708@gmail.com>
2022-05-13 21:25:32 -0400

    Added loading to prevent error

--- client/src/components/account/Account.js
+++ client/src/components/account/Account.js
@@ -9,6 +9,7 @@ import "../../App.css";
 
 const Account = () => {
     const [userData, setUserData] = useState(undefined);
+    const [loading, setLoading] = useState(true);
     const { currentUser } = useContext(AuthContext);
 
     let currentUserUid = currentUser.uid;
@@ -25,8 +26,10 @@ const Account = () => {
                 console.log(data);
 
                 setUserData(data);
+                setLoading(false);
             } catch (e) {
                 console.log(e);
+                setLoading(false);
             }
         }
         getUser();
@@ -35,20 +38,28 @@ const Account = () => {
     // let pastOrders = user.pastOrders;
 
     // console.log(userData);
-    return (
-        <div>
-            <h2>Account Page</h2>
-            <h3>{userData.name}</h3>
-            <img src={userData.image_url} alt="profile picture" />
-            <p>Address: {userData.address}</p>
-            <p>About me: {userData.biography}</p>
-            <Link to="/changePassword" className="link">
-                Change Password
-            </Link>
-            {/* <ChangePassword /> */}
-            <SignOutButton />
-        </div>
-    );
+    if (loading) {
+        return (
+            <div>
+                <h2>Loading....</h2>
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                <h2>Account Page</h2>
+                <h3>{userData.name}</h3>
+                <img src={userData.image_url} alt="profile picture" />
+                <p>Address: {userData.address}</p>
+                <p>About me: {userData.biography}</p>
+                <Link to="/changePassword" className="link">
+                    Change Password
+                </Link>
+                {/* <ChangePassword /> */}
+                <SignOutButton />
+            </div>
+        );
+    }
 };
 
 export default Account;
